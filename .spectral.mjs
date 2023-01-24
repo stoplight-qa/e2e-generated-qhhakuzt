399d69350e8a4c2910689e18a85f8163f1c3b37e
qa-rmnmg-31-qnvw@example.com <qa-rmnmg-31-qnvw@example.com>
2023-01-24 13:54:35 -0500

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "http://stoplight-local.com:8080/api/v1/projects/cHJqOjQxNA/spectral.js?branch_slug=master&token=4a35acdd-31b8-4b1d-9bcd-37237b0ec953";
+export default { extends: ruleset };
